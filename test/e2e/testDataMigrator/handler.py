4d8c12205d742547aa8a94c79b6555962781be54
emilyyoungss1 <emily.youngs@thoughtworks.com>
2021-08-17 17:09:40 +0100

    Successfully implemented a basic test for transferring a test file over datasync

--- test/e2e/testDataMigrator/handler.py
+++ test/e2e/testDataMigrator/handler.py
@@ -1,24 +1,36 @@
-import json
+import boto3
+import logging
+import polling
+from botocore.exceptions import ClientError
 
+REGION = "eu-west-2"
 
 def testMigrator(event, context):
-    body = {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "input": event
-    }
 
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(body)
-    }
+    try:
+        s3_client = boto3.client('s3', region_name=REGION)
+        s3_client.put_object(Body=bytearray("test", encoding="utf-8"), Bucket="pracmig-source-supplier-test-bucket", Key="test.txt")
+    except ClientError as e:
+        logging.error(e)
+        return False
 
-    return response
+    try:
+        datasync_client = boto3.client('datasync', region_name=REGION)
+        task_execution_response = datasync_client.start_task_execution(TaskArn="arn:aws:datasync:eu-west-2:327778747031:task/task-0ff150ad6e48c1d28")
+    except ClientError as e:
+        logging.error(e)
+        return False
 
-    # Use this code if you don't use the http event with the LAMBDA-PROXY
-    # integration
-    """
-    return {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "event": event
-    }
-    """
+    def check_success(response):
+        return response["Status"] == "SUCCESS"
+    polling.poll(lambda: datasync_client.describe_task_execution(TaskExecutionArn=task_execution_response["TaskExecutionArn"]),
+                 check_success=check_success, step=10, poll_forever=True)
+
+    try:
+        s3_client = boto3.client('s3', region_name=REGION)
+        response = s3_client.get_object(Bucket="pracmig-target-supplier-test-bucket", Key="test.txt")
+        logging.info(response)
+    except ClientError as e:
+        logging.error(e)
+        return False
+    return True
