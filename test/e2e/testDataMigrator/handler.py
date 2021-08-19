f52eaacb12ee104f4d5b855ddab45e16bc02bb3d
Mark Taylor <86659797+marktaylor80@users.noreply.github.com>
2021-08-19 14:08:57 +0100

    Validate transferred data
    Check that the data in the target bucket is the same as the data that
     was written to the source bucket.

--- test/e2e/testDataMigrator/handler.py
+++ test/e2e/testDataMigrator/handler.py
@@ -8,24 +8,36 @@ REGION = "eu-west-2"
 def testMigrator(event, context):
 
     try:
-        write_test_data_to_source_supplier_bucket()
+        source_data = "test"
+        write_test_data_to_source_supplier_bucket(source_data)
 
         transfer_files()
 
-        read_test_data_from_target_supplier_bucket()
+        target_data = read_test_data_from_target_supplier_bucket()
+
+        if source_data != target_data:
+            return {
+                "statusCode": 500,
+                "body": f"Data written ({source_data}) does not match data read ({target_data})"
+            }
+
+        return {
+            "statusCode": 200,
+            "body": f"Data written matches data read"
+        }
+
     except ClientError as e:
         logging.error(e)
         return {
             "statusCode": 500
         }
-    return {
-        "statusCode": 200
-    }
 
 def read_test_data_from_target_supplier_bucket():
     s3_client = boto3.client('s3', region_name=REGION)
     response = s3_client.get_object(Bucket="pracmig-target-supplier-test-bucket", Key="test.txt")
     logging.info(response)
+    data = response['Body'].read().decode("utf-8")
+    return data
 
 def transfer_files():
     datasync_client, task_execution_response = start_transfer()
@@ -40,6 +52,6 @@ def start_transfer():
     task_execution_response = datasync_client.start_task_execution(TaskArn="arn:aws:datasync:eu-west-2:327778747031:task/task-0ff150ad6e48c1d28")
     return datasync_client,task_execution_response
 
-def write_test_data_to_source_supplier_bucket():
+def write_test_data_to_source_supplier_bucket(data):
     s3_client = boto3.client('s3', region_name=REGION)
-    s3_client.put_object(Body=bytearray("test", encoding="utf-8"), Bucket="pracmig-source-supplier-test-bucket", Key="test.txt")
+    s3_client.put_object(Body=bytearray(data, encoding="utf-8"), Bucket="pracmig-source-supplier-test-bucket", Key="test.txt")
