0b7c4c0b5a77c1310a3975a55cdc637ab7fc4b06
emilyyoungss1 <emily.youngs@thoughtworks.com>
2021-08-24 15:48:23 +0100

    Refactored lambda code:
    refactored the test lambda to discover the source and target buckets from the datasync task instead of hardcoding the names

--- test/e2e/testDataMigrator/handler.py
+++ test/e2e/testDataMigrator/handler.py
@@ -4,16 +4,18 @@ import polling
 from botocore.exceptions import ClientError
 
 REGION = "eu-west-2"
+TASK_ARN = "arn:aws:datasync:eu-west-2:327778747031:task/task-077b1497521e705ea"
+
 
 def test_migrator(event, context):
 
     try:
         source_data = "test"
-        write_test_data_to_source_supplier_bucket(source_data)
+        write_test_data_to_source_supplier_bucket(source_data, TASK_ARN)
 
-        transfer_files()
+        transfer_files(TASK_ARN)
 
-        target_data = read_test_data_from_target_supplier_bucket()
+        target_data = read_test_data_from_target_supplier_bucket(TASK_ARN)
 
         if source_data != target_data:
             return {
@@ -32,26 +34,57 @@ def test_migrator(event, context):
             "statusCode": 500
         }
 
-def read_test_data_from_target_supplier_bucket():
+
+def extract_name_from_bucket_uri(location_uri):
+    location_uri = location_uri.split("//")
+    bucket_name = location_uri[1].replace("/", "")
+
+    return bucket_name
+
+
+def retrieve_bucket_name(task_arn, location_arn_key):
+    datasync_client = boto3.client('datasync', region_name=REGION)
+    task = datasync_client.describe_task(
+        TaskArn=task_arn
+    )
+
+    location_arn = task[location_arn_key]
+
+    location = datasync_client.describe_location_s3(
+        LocationArn=location_arn
+    )
+
+    bucket_name = extract_name_from_bucket_uri(location_uri=location["LocationUri"])
+
+    return bucket_name
+
+
+def read_test_data_from_target_supplier_bucket(task_arn):
+    bucket_name = retrieve_bucket_name(task_arn, location_arn_key="DestinationLocationArn")
     s3_client = boto3.client('s3', region_name=REGION)
-    response = s3_client.get_object(Bucket="pracmig-target-supplier-test-bucket", Key="test.txt")
+    response = s3_client.get_object(Bucket=bucket_name, Key="test.txt")
     logging.info(response)
     data = response['Body'].read().decode("utf-8")
     return data
 
-def transfer_files():
-    datasync_client, task_execution_response = start_transfer()
+
+def transfer_files(task_arn):
+    datasync_client, task_execution_response = start_transfer(task_arn)
 
     def check_success(response):
         return response["Status"] == "SUCCESS"
     polling.poll(lambda: datasync_client.describe_task_execution(TaskExecutionArn=task_execution_response["TaskExecutionArn"]),
                         check_success=check_success, step=10, poll_forever=True)
 
-def start_transfer():
-    datasync_client = boto3.client('datasync', region_name=REGION)
-    task_execution_response = datasync_client.start_task_execution(TaskArn="arn:aws:datasync:eu-west-2:327778747031:task/task-0ff150ad6e48c1d28")
-    return datasync_client,task_execution_response
 
-def write_test_data_to_source_supplier_bucket(data):
+def start_transfer(task_arn):
+    datasync_client = boto3.client('datasync', region_name=REGION)
+    task_execution_response = datasync_client.start_task_execution(TaskArn=task_arn)
+    return datasync_client, task_execution_response
+
+
+def write_test_data_to_source_supplier_bucket(data, task_arn):
+    bucket_name = retrieve_bucket_name(task_arn, location_arn_key="SourceLocationArn")
+
     s3_client = boto3.client('s3', region_name=REGION)
-    s3_client.put_object(Body=bytearray(data, encoding="utf-8"), Bucket="pracmig-source-supplier-test-bucket", Key="test.txt")
+    s3_client.put_object(Body=bytearray(data, encoding="utf-8"), Bucket=bucket_name, Key="test.txt")
