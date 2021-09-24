30db5d81836a43693e55bf087835fe2f1f587bea
Mark Taylor <86659797+marktaylor80@users.noreply.github.com>
2021-09-24 13:19:38 +0100

    PRACMIG-524: Estimate carbon footprint of AWS DataSync
    Calculates the estimated carbon footprint equivalent of the various
    components involved in the tactical solution for migrating a practice's
    data from one supplier to another.
    
    Uses the estimator classes from the code that makes up the Cloud Carbon
    Footprint tool (https://www.cloudcarbonfootprint.org/).

--- pracmig-co2-impact-calculator/README.md
+++ pracmig-co2-impact-calculator/README.md
@@ -1,26 +1,3 @@
-# @cloud-carbon-footprint/core
+# pracmig-co2-impact-calculator
 
-This package provides the core logic to get cloud usage data and estimate energy and carbon emissions.
-
-## Installation
-
-Install the package via npm or Yarn:
-
-```sh
-$ npm install --save @cloud-carbon-footprint/core
-```
-
-or
-
-```sh
-$ yarn add @cloud-carbon-footprint/core
-```
-
-## Documentation
-
-- [Cloud Carbon Footprint Readme](https://github.com/cloud-carbon-footprint/cloud-carbon-footprint/blob/trunk/README.md)
-- [Cloud Carbon Footprint Documentation](https://github.com/cloud-carbon-footprint/cloud-carbon-footprint/tree/trunk/microsite/docs/README.md)
-
-## License
-
-© 2021 Thoughtworks, Inc.
+Calculate the CO2 impact of typical usage
--- pracmig-co2-impact-calculator/src/__tests__/practice-migration-data-migration-pipeline-impact.test.ts
+++ pracmig-co2-impact-calculator/src/__tests__/practice-migration-data-migration-pipeline-impact.test.ts
@@ -2,10 +2,16 @@ import {
   AWS_CLOUD_CONSTANTS,
   AWS_EMISSIONS_FACTORS_METRIC_TON_PER_KWH,
 } from "@cloud-carbon-footprint/aws";
-import { NetworkingEstimator } from "@cloud-carbon-footprint/core";
+import {
+  ComputeEstimator,
+  COMPUTE_PROCESSOR_TYPES,
+  NetworkingEstimator,
+  StorageEstimator,
+} from "@cloud-carbon-footprint/core";
 
-describe("Calculate impact", () => {
-  it("should calculate network usage impact for a single migration", () => {
+describe("Impact calculator for a single migration", () => {
+  it("calculates network usage impact", () => {
+    // AWS_CLOUD_CONSTANTS.NETWORKING_COEFFICIENT is for internal AWS networking…
     const networkingCoefficient = AWS_CLOUD_CONSTANTS.NETWORKING_COEFFICIENT;
     const region = "eu-west-2";
     const emissionsFactors = AWS_EMISSIONS_FACTORS_METRIC_TON_PER_KWH;
@@ -16,8 +22,9 @@ describe("Calculate impact", () => {
       },
     ];
     const awsConstants = {
-      powerUsageEffectiveness: 1.135,
+      powerUsageEffectiveness: AWS_CLOUD_CONSTANTS.getPUE(region),
     };
+
     const result = new NetworkingEstimator(networkingCoefficient).estimate(
       input,
       region,
@@ -25,6 +32,110 @@ describe("Calculate impact", () => {
       awsConstants
     );
 
+    console.log("Network impact:");
+    console.dir(result, { depth: 4, colors: true });
+    expect(result).not.toBeNull();
+  });
+
+  it("calculates S3 storage usage impact", () => {
+    const networkingCoefficient = AWS_CLOUD_CONSTANTS.NETWORKING_COEFFICIENT;
+    const region = "eu-west-2";
+    const emissionsFactors = AWS_EMISSIONS_FACTORS_METRIC_TON_PER_KWH;
+    const input = [
+      {
+        // 5 days
+        terabyteHours: 5 * 24,
+      },
+    ];
+    const awsConstants = {
+      powerUsageEffectiveness: AWS_CLOUD_CONSTANTS.getPUE(region),
+    };
+
+    const result = new StorageEstimator(networkingCoefficient).estimate(
+      input,
+      region,
+      emissionsFactors,
+      awsConstants
+    );
+
+    console.log("Target supplier s3 bucket impact:");
+    console.dir(result, { depth: 4, colors: true });
+    expect(result).not.toBeNull();
+  });
+
+  it("calculates DataSync Agent usage impact", () => {
+    const region = "eu-west-2";
+    const emissionsFactors = AWS_EMISSIONS_FACTORS_METRIC_TON_PER_KWH;
+    const input = [
+      {
+        cpuUtilizationAverage: AWS_CLOUD_CONSTANTS.AVG_CPU_UTILIZATION_2020,
+        // 8 vCPUs (for an m5.2xlarge) * 24 hours * 5 days
+        numberOfvCpus: 8 * 24 * 5,
+        usesAverageCPUConstant: true,
+      },
+    ];
+    // COMPUTE_PROCESSOR_TYPES.SKYLAKE taken from INSTANCE_TYPE_COMPUTE_PROCESSOR_MAPPING['m5.2xlarge']
+    const awsConstants = {
+      minWatts: AWS_CLOUD_CONSTANTS.getMinWatts([
+        COMPUTE_PROCESSOR_TYPES.SKYLAKE,
+      ]),
+      maxWatts: AWS_CLOUD_CONSTANTS.getMaxWatts([
+        COMPUTE_PROCESSOR_TYPES.SKYLAKE,
+      ]),
+      powerUsageEffectiveness: AWS_CLOUD_CONSTANTS.getPUE(region),
+      // The agent will be on-premise so will likely not have replication
+      replicationFactor: 1,
+    };
+
+    const result = new ComputeEstimator().estimate(
+      input,
+      region,
+      emissionsFactors,
+      awsConstants
+    );
+
+    console.log("DataSync agent impact:");
+    console.dir(result, { depth: 4, colors: true });
+    expect(result).not.toBeNull();
+  });
+
+  it("calculates ACL updator lambda usage impact", () => {
+    const region = "eu-west-2";
+    const emissionsFactors = AWS_EMISSIONS_FACTORS_METRIC_TON_PER_KWH;
+    // 1 vCPU (for lambdas using less than 1792MB of memory)
+    const numberOfVCpus = 1;
+    // 1 second lambda execution time in hours
+    const lambdaExecutionDurationInHours = 1 / 3600;
+    // number of files per patient * number of patients
+    const numberOfFiles = 30 * 13000;
+    const input = [
+      {
+        cpuUtilizationAverage: AWS_CLOUD_CONSTANTS.AVG_CPU_UTILIZATION_2020,
+        numberOfvCpus:
+          numberOfVCpus * lambdaExecutionDurationInHours * numberOfFiles,
+        usesAverageCPUConstant: true,
+      },
+    ];
+    // using the same value as for an m5.2xlarge, since I can't find anything better to use
+    const awsConstants = {
+      minWatts: AWS_CLOUD_CONSTANTS.getMinWatts([
+        COMPUTE_PROCESSOR_TYPES.SKYLAKE,
+      ]),
+      maxWatts: AWS_CLOUD_CONSTANTS.getMaxWatts([
+        COMPUTE_PROCESSOR_TYPES.SKYLAKE,
+      ]),
+      powerUsageEffectiveness: AWS_CLOUD_CONSTANTS.getPUE(region),
+      replicationFactor: 1,
+    };
+
+    const result = new ComputeEstimator().estimate(
+      input,
+      region,
+      emissionsFactors,
+      awsConstants
+    );
+
+    console.log("ACL lambda usage:");
     console.dir(result, { depth: 4, colors: true });
     expect(result).not.toBeNull();
   });
