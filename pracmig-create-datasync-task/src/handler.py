a61fa3d3a0c0529ac37a29d9ed29a540ae93dd9a
Mark Taylor <86659797+marktaylor80@users.noreply.github.com>
2021-11-05 16:02:32 +0000

    PRACMIG-513: Refactor handler to use crhelper
    Amazon provide a handy library to ease implementing custom lambda
     resources - make use of it!

--- pracmig-create-datasync-task/src/handler.py
+++ pracmig-create-datasync-task/src/handler.py
@@ -1,23 +1,52 @@
 import json
-import cfnresponse
+from crhelper import CfnResource
+import logging
 import os
 from urllib.parse import urlparse, parse_qs
 import requests
-import logging
+
+
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
+
+helper = CfnResource(
+    json_logging=False,
+    log_level='DEBUG',
+    boto_level='CRITICAL'
+)
+
+
 def handler(event, context):
-    logging.info("REQUEST RECEIVED:\n" + json.dumps(event))
+    helper(event, context)
+
+@helper.create
+def create(event, context):
+    logger.info("Create request received:\n" + json.dumps(event))
+
     try:
         AGENT_IP = os.environ['AGENT_IP']
-        key = {}
+        key = ""
         if event["RequestType"] in ["Create", "Update"]:
             logger.info(f'Requesting Activation Key: http://{AGENT_IP}/?activationRegion=eu-west-2')
             res = requests.get(f'http://{AGENT_IP}/?activationRegion=eu-west-2', timeout=2)
             res.raise_for_status()
             key = parse_qs(urlparse(res.history[1].url).query)['activationKey'][0]
-        cfnresponse.send(event, context, cfnresponse.SUCCESS, {"ActivationKey": key})
+        # Items stored in helper.Data will be saved 
+        # as outputs in your resource in CloudFormation
+        helper.Data.update({"ActivationKey": key})
+        return "DataSyncActivationKey"
     except requests.exceptions.RequestException as e:
         logger.error(f"Unable to activate agent", exc_info=1)
         raise
+
+
+@helper.update
+def update(event, context):
+    logger.info("Update request received:\n" + json.dumps(event))
+    return "DataSyncActivationKey"
+
+
+@helper.delete
+def delete(event, context):
+    logger.info("Delete request received:\n" + json.dumps(event))
--- pracmig-create-datasync-task/tests/unit/test_handler.py
+++ pracmig-create-datasync-task/tests/unit/test_handler.py
@@ -1,27 +1,75 @@
-import os
-import sys
-
-import boto3
-import pytest
-from moto.core import set_initial_no_auth_action_count
-import urllib
+from mock.mock import ANY
+from hamcrest import *
 from src.handler import handler
 from mock import Mock, MagicMock
-import cfnresponse
+import json
 
 
-def test_handler_returns_activation_key(monkeypatch):
-    monkeypatch.setenv('AGENT_IP', '18.170.222.23')
-    cf_mock = Mock()
-    mock_agent_call = Mock(history=['', Mock(url='http://mock-redirect/?activationKey=KEY_00000')])
-    monkeypatch.setattr('cfnresponse.send', cf_mock)
-    monkeypatch.setattr('requests.get', lambda url: mock_agent_call)
+def test_handler_sends_activation_key_to_cloud_formation_when_creating(monkeypatch):
+    activation_key = "KEY_00000"
+    response_url = "/custom-resource-response-url"
+    stack_id = "arn:aws:cloudformation:eu-west-2:123456789012:stack/mystack-mynestedstack-sggfrhxhum7w/f449b250-b969-11e0-a185-5081d0136786";
+    request_id = "test-request-id"
+    logical_resource_id = "test-logical-resource-id"
+    monkeypatch.setenv("AGENT_IP", "18.170.222.23")
+    cf_request_mock = MagicMock()
+    cf_call_mock = MagicMock(return_value=Mock(request=cf_request_mock, getresponse=lambda: Mock(status=200, reason="some reason")))
+    monkeypatch.setattr("crhelper.utils.HTTPSConnection", cf_call_mock)
+    mock_agent_call = Mock(history=["", Mock(url=f"http://mock-redirect/?activationKey={activation_key}")])
+    monkeypatch.setattr("requests.get", lambda url, timeout: mock_agent_call)
     event = {
         "RequestType": "Create",
-        'ResponseURL': 'https://'
+        "ResponseURL": f"https://aws-cloud-formation-host.com{response_url}",
+        "StackId": stack_id,
+        "RequestId": request_id,
+        "LogicalResourceId": logical_resource_id
     }
-    context = {
-        "log_stream_name": "blah"
-    }
+    context = Mock(
+        log_stream_name="blah",
+        get_remaining_time_in_millis=lambda: 5000
+    )
+
     handler(event, context)
-    cf_mock.assert_called_with(event, context, 'SUCCESS', 'KEY_00000', 'ok')
+
+    cf_request_mock.assert_called_with(method="PUT", url=response_url, body=ANY, headers=ANY)
+    deserialised_body = json.loads(cf_request_mock.call_args.kwargs["body"])
+    assert_that(deserialised_body, has_entry("Status", "SUCCESS"))
+    assert_that(deserialised_body, has_entry("StackId", stack_id))
+    assert_that(deserialised_body, has_entry("RequestId", request_id))
+    assert_that(deserialised_body, has_entry("LogicalResourceId", logical_resource_id))
+    assert_that(deserialised_body, has_entry("Data", has_entry("ActivationKey", activation_key)))
+
+
+def test_handler_sends_activation_key_to_cloud_formation_when_deleting(monkeypatch):
+    activation_key = "KEY_00000"
+    response_url = "/custom-resource-response-url"
+    stack_id = "arn:aws:cloudformation:eu-west-2:123456789012:stack/mystack-mynestedstack-sggfrhxhum7w/f449b250-b969-11e0-a185-5081d0136786";
+    request_id = "test-request-id"
+    logical_resource_id = "test-logical-resource-id"
+    monkeypatch.setenv("AGENT_IP", "18.170.222.23")
+    cf_request_mock = MagicMock()
+    cf_call_mock = MagicMock(return_value=Mock(request=cf_request_mock, getresponse=lambda: Mock(status=200, reason="some reason")))
+    monkeypatch.setattr("crhelper.utils.HTTPSConnection", cf_call_mock)
+    mock_agent_call = Mock(history=["", Mock(url=f"http://mock-redirect/?activationKey={activation_key}")])
+    monkeypatch.setattr("requests.get", lambda url, timeout: mock_agent_call)
+    event = {
+        "RequestType": "Delete",
+        "ResponseURL": f"https://aws-cloud-formation-host.com{response_url}",
+        "StackId": stack_id,
+        "RequestId": request_id,
+        "LogicalResourceId": logical_resource_id
+    }
+    context = Mock(
+        log_stream_name="blah",
+        get_remaining_time_in_millis=lambda: 5000
+    )
+
+    handler(event, context)
+
+    cf_request_mock.assert_called_with(method="PUT", url=response_url, body=ANY, headers=ANY)
+    deserialised_body = json.loads(cf_request_mock.call_args.kwargs["body"])
+    assert_that(deserialised_body, has_entry("Status", "SUCCESS"))
+    assert_that(deserialised_body, has_entry("StackId", stack_id))
+    assert_that(deserialised_body, has_entry("RequestId", request_id))
+    assert_that(deserialised_body, has_entry("LogicalResourceId", logical_resource_id))
+    assert_that(deserialised_body, has_entry("Data", empty()))
