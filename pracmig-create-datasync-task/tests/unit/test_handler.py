5ab03d44a1fabd2c5e8052d7946d1cb8aa56101d
Mark Taylor <86659797+marktaylor80@users.noreply.github.com>
2021-11-19 15:44:53 +0000

    PRACMIG-513: Test failure notification to CF

--- pracmig-create-datasync-task/tests/unit/test_handler.py
+++ pracmig-create-datasync-task/tests/unit/test_handler.py
@@ -84,6 +84,31 @@ def test_handler_retries_activation_call(monkeypatch):
     assert_that(deserialised_body, has_entry("Status", "SUCCESS"))
 
 
+def test_handler_sends_failure_notification_to_cf_when_agent_ip_not_set(monkeypatch):
+    cf_request_mock = MagicMock()
+    cf_call_mock = MagicMock(return_value=Mock(
+        request=cf_request_mock, getresponse=lambda: Mock(status=200, reason="some reason")))
+    monkeypatch.setattr("crhelper.utils.HTTPSConnection", cf_call_mock)
+    event = {
+        "RequestType": "Create",
+        "ResponseURL": f"https://aws-cloud-formation-host.com/custom-resource-response-url",
+        "StackId": "arn:aws:cloudformation:eu-west-2:123456789012:stack/mystack-mynestedstack-sggfrhxhum7w/f449b250-b969-11e0-a185-5081d0136786",
+        "RequestId": "test-request-id",
+        "LogicalResourceId": "test-logical-resource-id"
+    }
+    context = Mock(
+        log_stream_name="blah",
+        get_remaining_time_in_millis=lambda: 5000
+    )
+
+    handler(event, context)
+
+    cf_request_mock.assert_called_with(
+        method="PUT", url=ANY, body=ANY, headers=ANY)
+    deserialised_body = json.loads(cf_request_mock.call_args.kwargs["body"])
+    assert_that(deserialised_body, has_entry("Status", "FAILED"))
+
+
 def test_handler_sends_activation_key_to_cloud_formation_when_deleting(monkeypatch):
     response_url = "/custom-resource-response-url"
     stack_id = "arn:aws:cloudformation:eu-west-2:123456789012:stack/mystack-mynestedstack-sggfrhxhum7w/f449b250-b969-11e0-a185-5081d0136786"
