9f6a71ac68d52a06ce67df9883ab4d263befd213
Mark Taylor <86659797+marktaylor80@users.noreply.github.com>
2021-11-19 16:48:34 +0000

    PRACMIG-513: Refactor tests
    Extract the lambda event to a Pytest fixture.

--- pracmig-create-datasync-task/tests/unit/test_handler.py
+++ pracmig-create-datasync-task/tests/unit/test_handler.py
@@ -6,6 +6,8 @@ from mock import Mock, MagicMock
 import json
 import pytest
 
+CUSTOM_RESPONSE_PATH = "/custom-resource-response-url"
+
 
 @pytest.fixture
 def cf_request_mock(monkeypatch):
@@ -24,44 +26,58 @@ def lambda_context():
     )
 
 
+@pytest.fixture
+def create_event():
+    return {
+        "RequestType": "Create",
+        "ResponseURL": f"https://aws-cloud-formation-host.com{CUSTOM_RESPONSE_PATH}",
+        "StackId": "arn:aws:cloudformation:eu-west-2:123456789012:stack/mystack-mynestedstack-sggfrhxhum7w/f449b250-b969-11e0-a185-5081d0136786",
+        "RequestId": "test-request-id",
+        "LogicalResourceId": "test-logical-resource-id"
+    }
+
+
+@pytest.fixture
+def delete_event():
+    return {
+        "RequestType": "Delete",
+        "ResponseURL": f"https://aws-cloud-formation-host.com{CUSTOM_RESPONSE_PATH}",
+        "StackId": "arn:aws:cloudformation:eu-west-2:123456789012:stack/mystack-mynestedstack-sggfrhxhum7w/f449b250-b969-11e0-a185-5081d0136786",
+        "RequestId": "test-request-id",
+        "LogicalResourceId": "test-logical-resource-id"
+    }
+
+
 def test_handler_sends_activation_key_to_cloud_formation_when_creating(
-        monkeypatch, cf_request_mock, lambda_context):
+        monkeypatch, cf_request_mock, create_event, lambda_context):
     activation_key = "KEY_00000"
-    response_url = "/custom-resource-response-url"
-    stack_id = "arn:aws:cloudformation:eu-west-2:123456789012:stack/mystack-mynestedstack-sggfrhxhum7w/f449b250-b969-11e0-a185-5081d0136786"
-    request_id = "test-request-id"
-    logical_resource_id = "test-logical-resource-id"
     monkeypatch.setenv("AGENT_IP", "35.179.77.230")
     mock_key_request_conn = Mock(getresponse=lambda: Mock(
         getheader=lambda _: f"http://mock-redirect/?activationKey={activation_key}"))
     monkeypatch.setattr(
         "src.handler.HTTPConnection",
         lambda host, port, timeout: mock_key_request_conn)
-    event = {
-        "RequestType": "Create",
-        "ResponseURL": f"https://aws-cloud-formation-host.com{response_url}",
-        "StackId": stack_id,
-        "RequestId": request_id,
-        "LogicalResourceId": logical_resource_id
-    }
 
-    handler(event, lambda_context)
+    handler(create_event, lambda_context)
 
     cf_request_mock.assert_called_with(
-        method="PUT", url=response_url, body=ANY, headers=ANY)
+        method="PUT", url=CUSTOM_RESPONSE_PATH, body=ANY, headers=ANY)
     deserialised_body = json.loads(cf_request_mock.call_args.kwargs["body"])
     assert_that(deserialised_body, has_entry("Status", "SUCCESS"))
-    assert_that(deserialised_body, has_entry("StackId", stack_id))
-    assert_that(deserialised_body, has_entry("RequestId", request_id))
+    assert_that(deserialised_body, has_entry(
+        "StackId", create_event['StackId']))
+    assert_that(deserialised_body, has_entry(
+        "RequestId", create_event['RequestId']))
     assert_that(
-        deserialised_body, has_entry("LogicalResourceId", logical_resource_id))
+        deserialised_body,
+        has_entry("LogicalResourceId", create_event["LogicalResourceId"]))
     assert_that(
         deserialised_body,
         has_entry("Data", has_entry("ActivationKey", activation_key)))
 
 
 def test_handler_retries_activation_call(
-        monkeypatch, cf_request_mock, lambda_context):
+        monkeypatch, cf_request_mock, create_event, lambda_context):
     monkeypatch.setenv("AGENT_IP", "35.179.77.230")
     mock_activation_key_request = MagicMock(side_effect=[HTTPException, None])
     mock_key_request_conn = Mock(
@@ -71,15 +87,8 @@ def test_handler_retries_activation_call(
     monkeypatch.setattr(
         "src.handler.HTTPConnection",
         lambda host, port, timeout: mock_key_request_conn)
-    event = {
-        "RequestType": "Create",
-        "ResponseURL": f"https://aws-cloud-formation-host.com/custom-resource-response-url",
-        "StackId": "arn:aws:cloudformation:eu-west-2:123456789012:stack/mystack-mynestedstack-sggfrhxhum7w/f449b250-b969-11e0-a185-5081d0136786",
-        "RequestId": "test-request-id",
-        "LogicalResourceId": "test-logical-resource-id"
-    }
 
-    handler(event, lambda_context)
+    handler(create_event, lambda_context)
 
     assert_that(mock_activation_key_request.call_args_list, has_length(2))
     cf_request_mock.assert_called_with(
@@ -89,16 +98,9 @@ def test_handler_retries_activation_call(
 
 
 def test_handler_sends_failure_notification_to_cf_when_agent_ip_not_set(
-        cf_request_mock, lambda_context):
-    event = {
-        "RequestType": "Create",
-        "ResponseURL": f"https://aws-cloud-formation-host.com/custom-resource-response-url",
-        "StackId": "arn:aws:cloudformation:eu-west-2:123456789012:stack/mystack-mynestedstack-sggfrhxhum7w/f449b250-b969-11e0-a185-5081d0136786",
-        "RequestId": "test-request-id",
-        "LogicalResourceId": "test-logical-resource-id"
-    }
+        cf_request_mock, create_event, lambda_context):
 
-    handler(event, lambda_context)
+    handler(create_event, lambda_context)
 
     cf_request_mock.assert_called_with(
         method="PUT", url=ANY, body=ANY, headers=ANY)
@@ -107,27 +109,21 @@ def test_handler_sends_failure_notification_to_cf_when_agent_ip_not_set(
 
 
 def test_handler_sends_activation_key_to_cloud_formation_when_deleting(
-        cf_request_mock, lambda_context):
-    response_url = "/custom-resource-response-url"
-    stack_id = "arn:aws:cloudformation:eu-west-2:123456789012:stack/mystack-mynestedstack-sggfrhxhum7w/f449b250-b969-11e0-a185-5081d0136786"
-    request_id = "test-request-id"
-    logical_resource_id = "test-logical-resource-id"
-    event = {
-        "RequestType": "Delete",
-        "ResponseURL": f"https://aws-cloud-formation-host.com{response_url}",
-        "StackId": stack_id,
-        "RequestId": request_id,
-        "LogicalResourceId": logical_resource_id
-    }
+        cf_request_mock, delete_event, lambda_context):
 
-    handler(event, lambda_context)
+    handler(delete_event, lambda_context)
 
     cf_request_mock.assert_called_with(
-        method="PUT", url=response_url, body=ANY, headers=ANY)
+        method="PUT", url=CUSTOM_RESPONSE_PATH, body=ANY, headers=ANY)
     deserialised_body = json.loads(cf_request_mock.call_args.kwargs["body"])
     assert_that(deserialised_body, has_entry("Status", "SUCCESS"))
-    assert_that(deserialised_body, has_entry("StackId", stack_id))
-    assert_that(deserialised_body, has_entry("RequestId", request_id))
     assert_that(
-        deserialised_body, has_entry("LogicalResourceId", logical_resource_id))
+        deserialised_body,
+        has_entry("StackId", delete_event["StackId"]))
+    assert_that(
+        deserialised_body,
+        has_entry("RequestId", delete_event["RequestId"]))
+    assert_that(
+        deserialised_body,
+        has_entry("LogicalResourceId", delete_event["LogicalResourceId"]))
     assert_that(deserialised_body, has_entry("Data", empty()))
