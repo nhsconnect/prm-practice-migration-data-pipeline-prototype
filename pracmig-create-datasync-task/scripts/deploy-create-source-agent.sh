9e6a89ff6a991f90b315779b209ba390d7aed5e0
Mark Taylor <86659797+marktaylor80@users.noreply.github.com>
2021-11-23 16:39:17 +0000

    PRACMIG-513: Add a bastion to source supplier CF
    Add a Bastion host / jump box to the source supplier VPC for the
     purposes of SSH'ing to it so that we can manually place some test files
     on the NFS server to test the DataSync transfer.
    
    Add a CIDR block parameter to specify the IP addresses that will be
     allowed to connect to the Bastion.
    
    Caius / Mark

--- pracmig-create-datasync-task/scripts/deploy-create-source-agent.sh
+++ pracmig-create-datasync-task/scripts/deploy-create-source-agent.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 # Helper for creating the source agent stack
 
-while getopts i: flag
+while getopts i:c: flag
 do
     case "${flag}" in
+        c) CidrBlockAllowedAccessToBastion=${OPTARG};;
         i) id=${OPTARG};;
     esac
 done
 
 aws cloudformation create-stack --template-body "$(cat ./packaged-source-supplier.yml)" \
+  --parameters ParameterKey=CidrBlockAllowedAccessToBastion,ParameterValue=$CidrBlockAllowedAccessToBastion \
   --timeout-in-minutes 30 \
   --stack-name "source-supplier-$id" \
   --disable-rollback \
