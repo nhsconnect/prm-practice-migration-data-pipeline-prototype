5792fa092b606f8596ff4b0e03524ddb1d3165b3
Mark Taylor <86659797+marktaylor80@users.noreply.github.com>
2021-11-24 11:16:16 +0000

    PRACMIG-513: Add a key-pair for bastion access
    Add a parameter to specify the key-pair to be enabled for SSH access to
     the bastion host.

--- pracmig-create-datasync-task/scripts/deploy-create-source-agent.sh
+++ pracmig-create-datasync-task/scripts/deploy-create-source-agent.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 # Helper for creating the source agent stack
 
-while getopts i:c: flag
+while getopts c:i:k: flag
 do
     case "${flag}" in
         c) CidrBlockAllowedAccessToBastion=${OPTARG};;
         i) id=${OPTARG};;
+        k) BastionKeyName=${OPTARG};;
     esac
 done
 
 aws cloudformation create-stack --template-body "$(cat ./packaged-source-supplier.yml)" \
-  --parameters ParameterKey=CidrBlockAllowedAccessToBastion,ParameterValue=$CidrBlockAllowedAccessToBastion \
+  --parameters ParameterKey=CidrBlockAllowedAccessToBastion,ParameterValue="${CidrBlockAllowedAccessToBastion}" \
+               ParameterKey=BastionKeyName,ParameterValue="${BastionKeyName}" \
   --timeout-in-minutes 30 \
   --stack-name "source-supplier-$id" \
   --disable-rollback \
