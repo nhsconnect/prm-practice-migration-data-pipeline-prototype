99e92ddf1ef533a548b516d2a7c864b081a25d34
Mark Taylor <86659797+marktaylor80@users.noreply.github.com>
2021-11-25 16:06:12 +0000

    PRACMIG-513: Non-functional shell script changes
    Use good practice in the shell scripts of surrounding variables in
     quotes and using curly braces.

--- pracmig-create-datasync-task/scripts/deploy-datasync-task.sh
+++ pracmig-create-datasync-task/scripts/deploy-datasync-task.sh
@@ -14,11 +14,11 @@ done
 aws cloudformation create-stack \
   --template-body "$(cat ./datasync.yml)"\
   --timeout-in-minutes 30 \
-  --stack-name "datasync-migration-$OdsCode" \
-  --parameters ParameterKey=OdsCode,ParameterValue=$OdsCode\
-               ParameterKey=DataSyncAgentArn,ParameterValue=$DataSyncAgentArn\
-               ParameterKey=SourceNfsServer,ParameterValue=$SourceNfsServer\
-               ParameterKey=SourceNfsPath,ParameterValue=$SourceNfsPath\
-               ParameterKey=TargetS3BucketArn,ParameterValue=$TargetS3BucketArn\
+  --stack-name "datasync-migration-${OdsCode}" \
+  --parameters ParameterKey=OdsCode,ParameterValue="${OdsCode}"\
+               ParameterKey=DataSyncAgentArn,ParameterValue="${DataSyncAgentArn}"\
+               ParameterKey=SourceNfsServer,ParameterValue="${SourceNfsServer}"\
+               ParameterKey=SourceNfsPath,ParameterValue="${SourceNfsPath}"\
+               ParameterKey=TargetS3BucketArn,ParameterValue="${TargetS3BucketArn}"\
   --disable-rollback \
   --capabilities CAPABILITY_AUTO_EXPAND CAPABILITY_NAMED_IAM
--- pracmig-create-datasync-task/scripts/deploy-register-agent.sh
+++ pracmig-create-datasync-task/scripts/deploy-register-agent.sh
@@ -12,7 +12,7 @@ done
 aws cloudformation create-stack --template-body "$(cat ./register-datasync-agent.yml)" \
   --capabilities CAPABILITY_NAMED_IAM \
   --timeout-in-minutes 30 \
-  --stack-name "register-datasync-agent-$ods" \
+  --stack-name "register-datasync-agent-${ods}" \
   --disable-rollback \
-  --parameters ParameterKey=OdsCode,ParameterValue=$ods \
-               ParameterKey=DataSyncAgentActivationKey,ParameterValue=$key
+  --parameters ParameterKey=OdsCode,ParameterValue="${ods}" \
+               ParameterKey=DataSyncAgentActivationKey,ParameterValue="${key}"
